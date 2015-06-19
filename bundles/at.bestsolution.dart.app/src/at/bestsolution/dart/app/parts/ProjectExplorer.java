506e85ae100855005859825a1cac767ac0ad8cf0
Tom Schindl <tom.schindl@bestsolution.at>
2015-06-19 12:25:21 +0200

    default state

--- bundles/at.bestsolution.dart.app/src/at/bestsolution/dart/app/parts/ProjectExplorer.java
+++ bundles/at.bestsolution.dart.app/src/at/bestsolution/dart/app/parts/ProjectExplorer.java
@@ -3,6 +3,7 @@ package at.bestsolution.dart.app.parts;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
+import javax.annotation.PostConstruct;
 import javax.inject.Inject;
 
 import org.eclipse.e4.ui.model.application.MApplication;
@@ -10,15 +11,19 @@ import org.eclipse.e4.ui.model.application.ui.basic.MPart;
 import org.eclipse.e4.ui.model.application.ui.basic.MPartStack;
 import org.eclipse.e4.ui.workbench.modeling.EModelService;
 import org.eclipse.e4.ui.workbench.modeling.EPartService;
+import org.eclipse.fx.core.URI;
 import org.eclipse.fx.ui.controls.filesystem.FileItem;
 import org.eclipse.fx.ui.controls.filesystem.ResourceEvent;
 import org.eclipse.fx.ui.controls.filesystem.ResourceItem;
 import org.eclipse.fx.ui.controls.filesystem.ResourceTreeView;
+import org.eclipse.fx.ui.services.resources.GraphicsLoader;
 
 import at.bestsolution.dart.server.api.DartServer;
 import at.bestsolution.dart.server.api.services.ServiceAnalysis;
 import javafx.collections.FXCollections;
+import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 
 public class ProjectExplorer {
 	@Inject
@@ -29,8 +34,13 @@ public class ProjectExplorer {
 	
 	MPartStack partStack;
 	
-	@Inject
-	public void init(BorderPane p, DartServer server, MApplication application) {
+	@PostConstruct
+	public void init(BorderPane p, DartServer server, MApplication application, GraphicsLoader nodeProvider) {
+		HBox b = new HBox();
+		b.getStyleClass().add("tool-bar");
+		b.getChildren().add(new Label("Project Explorer", nodeProvider.getGraphicsNode(URI.createPlatformPluginURI("at.bestsolution.dart.app","css/icons/16/filenav_nav.png"))));
+		p.setTop(b);
+		
 		partStack = (MPartStack) modelService.find("at.bestsolution.dart.app.editorstack", application);
 		server.getService(ServiceAnalysis.class).setAnalysisRoots(new String[] {"/Users/tomschindl/dart-samples/"}, new String[0], null);
 		ResourceTreeView viewer = new ResourceTreeView();
